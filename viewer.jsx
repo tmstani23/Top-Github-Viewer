f0e349a326d73458aa17397ecadab00f01532adf
tmstani23 <tmstani23@gmail.com>
2018-06-10 14:13:45 -0500

    Update 9
    Cleaned up the code.  Added comments.  Added screenshots.

--- viewer.jsx
+++ viewer.jsx
@@ -27,7 +27,7 @@ class Loading extends React.Component {
     }
 }
 
-
+//Navbar component that displays the languages and buttons
 function Nav(props) {
     const languages = ["All", "Javascript", "Ruby", "Python"];
 
@@ -43,13 +43,15 @@ function Nav(props) {
         </nav>
     )
 }
-
+//Grid component that displays all the repositories and the names and star count
 function RepoGrid(props) {
     return (
         <ul className="repo-grid">
+            {/* The data from the api is mapped into the repos array */}
             {props.repos.map(( { name, owner, stargazers_count, html_url } ) => (
                 <li key = { name } >
                     <ul>
+                        {/* Here each piece of the data is mapped to a list element and displayed */}
                         <li><a href={ html_url }> {name} </a></li>
                         <li>@{ owner.login }</li>
                         <li id="stargazers-li">{ stargazers_count }</li>
@@ -74,12 +76,12 @@ class App extends React.Component {
         this.handleSelectLanguage = this.handleSelectLanguage.bind(this);
         this.fetchRepos = this.fetchRepos.bind(this);
     }
-
+    //This method sets the loading state and collects the data from the api
     fetchRepos(lang) {
         this.setState( {
             loading: true,
         } )
-
+        //Here the data is fetched from the api and the loading state and repos array are updated:
         window.API.fetchPopularRepos(lang)
             .then((repos) => {
                 this.setState( {
@@ -88,11 +90,11 @@ class App extends React.Component {
                 } )
             } )
     }
-
+    //Call fetch the repos for each language once the component mounts
     componentDidMount() {
         this.fetchRepos(this.state.activeLanguage);
     }
-
+    //Update the component with the current active language if it is different than the previous one
     componentDidUpdate(prevProps, prevState) {
         if(prevState.activeLanguage !== this.state.activeLanguage) {
             this.fetchRepos(this.state.activeLanguage);
@@ -104,12 +106,15 @@ class App extends React.Component {
             activeLanguage: lang
         } ) 
     }
+    //Render the Nav, Loading and RepoGrid components:
     render() {
         return (
         <div>
             <Nav onSelectLanguage = { this.handleSelectLanguage }/>
+            {/* If loading is true display loading component */}
             { this.state.loading === true 
                 ? <Loading />
+                // Else display a header with the current language and the RepoGrid component
                 : <div>
                     <h1 id = "lang-header">
                     { this.state.activeLanguage }
